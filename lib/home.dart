45cebf0c5bab7229749e4a78b733962d11dae432
Naynesh-Patel <46416205+Naynesh-Patel@users.noreply.github.com>
2023-07-11 10:28:03 +0530

    commit

--- lib/home.dart
+++ lib/home.dart
@@ -91,6 +91,7 @@ class _HomeState extends State<Home> {
                         enabledBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(5.0),
                             borderSide: const BorderSide(color: Colors.black)
+
                         ),
                       ),
                     ),
